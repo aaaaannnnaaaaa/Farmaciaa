a066a27a118574a5850f76557ec44c9d2742ad88
aaaaannnnaaaaa <ana.mena.dev2024@gmail.com>
2024-11-04 15:01:35 -0300

    Add files via upload

---
+++ prjFarmacia/src/main/java/com/example/demo/services/FornecedorService.java
@@ -0,0 +1,29 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import com.example.demo.entities.Fornecedor;
+
+public class FornecedorService {
+
+	public Fornecedor salvarFarmacia(Long fornecedor) {
+		return null;
+	}
+
+	public List<Fornecedor> buscarTodosFornecedor() {
+		return null;
+	}
+
+	public Fornecedor buscarMedicamentoPorId(Long id) {
+		return null;
+	}
+
+	public void deletarMedicamento(Long id) {
+		
+	}
+
+	public Fornecedor atualizarFornecedor(Long id, Fornecedor fornecedor) {
+		return null;
+	}
+
+}
